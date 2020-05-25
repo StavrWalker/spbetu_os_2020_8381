6fac6e9e0b7cde47d6f31296cbf40e17c8ee3804
caxapexac <caxapexac@gmail.com>
2020-05-26 00:01:59 +0300

    LR5 Added

---
+++ sakharov/lab5/build.bat
@@ -0,0 +1,13 @@
+
+rem ..\masm\masm src_f;
+rem ..\masm\link src_f;
+rem ..\masm\exe2bin src_f bin_f.com;
+
+
+..\masm\masm inter;
+..\masm\link inter;
+rem ..\masm\exe2bin inter bin_int.com;
+
+
+
+pause
